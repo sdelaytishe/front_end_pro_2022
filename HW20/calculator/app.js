f24dcf4082a608e2031310d2b0507bdc1b28d134
sdelaytishe <sdelaytishe@gmail.com>
2022-11-22 14:30:52 +0200

    HW20

---
+++ HW20/calculator/app.js
@@ -0,0 +1,6 @@
+const calculator = require('./index');
+
+console.log(calculator.add(2,3));
+console.log(calculator.sub(10,7));
+console.log(calculator.mult(10,3));
+console.log(calculator.div(50,2));
